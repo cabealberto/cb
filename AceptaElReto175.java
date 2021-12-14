019f88dc27644c4bf04ea66c96c5af2f49b0b9e5
cabealberto <95477787+cabealberto@users.noreply.github.com>
2021-12-14 17:52:03 +0100

    Update and rename Pastillas.java to AceptaElReto175.java

--- AceptaElReto175.java
+++ AceptaElReto175.java
@@ -1,4 +1,8 @@
 /*
+Resolucion del problema de aceptaelreto.com numero 175
+https://www.aceptaelreto.com/pub/problems/v001/75/st/statements/Spanish/index.html
+
+
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
