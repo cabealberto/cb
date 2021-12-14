f0f0a6931074b903fc22ea3c1c06a481a38e3f80
cabealberto <95477787+cabealberto@users.noreply.github.com>
2021-12-14 17:45:56 +0100

    Update Años.java

--- Años.java
+++ Años.java
@@ -1,4 +1,11 @@
 /*
+
+//Problemas acepta el reto
+https://www.aceptaelreto.com/problem/statement.php?id=174
+
+Mary lleva muchos años redactando cartas para sus jefes. Cuando comenzó el año 2013, observó con fastidio que al poner la fecha se veía obligada a utilizar más teclas diferentes que otras veces. Empezó a hacer memoria y se dio cuenta de que, tras 25 años, el año 2013 era el primero que tenía sus cuatro dígitos diferentes. Desde 1988, todos los años habían tenido al menos un dígito repetido.
+
+La serie de años sin dígitos repetidos que comienza con 2013 terminará en 2019. En 2020 comenzará una serie nueva de números con dígitos repetidos, que se mantendrá hasta el 2030, incluído.
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
